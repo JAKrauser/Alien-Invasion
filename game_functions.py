39d58fd6ecd2774e42215192829d7dca87f1e4a2
Josh <krauser.ja@gmail.com>
2018-01-15 16:54:14 -0500

    blurg

--- game_functions.py
+++ game_functions.py
@@ -1,4 +1,5 @@
 import sys 
+from time import sleep
 import pygame
 
 from bullet import Bullet
@@ -48,7 +49,7 @@ def update_screen(ai_settings, screen, ship, aliens, bullets):
 	# Makes most recently drawn screen visible
 	pygame.display.flip()
 
-def update_bullets(bullets):
+def update_bullets(ai_settings, screen, ship, aliens, bullets):
 	#delete old bullets
 	bullets.update()
 
@@ -56,14 +57,92 @@ def update_bullets(bullets):
 		if bullet.rect.bottom <= 0:
 			bullets.remove(bullet)
 
-def create_fleet(ai_settings, screen, aliens):
-	alien = Alien(ai_settings, screen)
-	alien_width = alien.rect.width
+	check_bullet_alien_collision(ai_settings, screen, ship, aliens, bullets)
+
+def check_bullet_alien_collision(ai_settings, screen, ship, aliens, bullets):
+	#remove bullets and aliens that have collided
+	collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+
+	if len(aliens) == 0:
+		#destroy exsisting bullets and create new fleet
+		bullets.empty()
+		create_fleet(ai_settings, screen, ship, aliens)
+			
+def get_number_aliens_x(ai_settings, alien_width):
+	#determine number of aliens that fit in a row
 	available_space_x = ai_settings.screen_width - 2 * alien_width
 	number_aliens_x = int(available_space_x / (2 * alien_width))
+	return number_aliens_x
 
-	for alien_number in range(number_aliens_x):
-		alien = Alien(ai_settings, screen)
-		alien.x = alien_width + 2 * alien_width * alien_number
-		alien.rect.x = alien.x
-		aliens.add(alien)
+def get_number_rows(ai_settings, ship_height, alien_height):
+	#determine number of rows of aliens that fit on screen
+	available_space_y = (ai_settings.screen_height - 
+							(3 * alien_height) - ship_height)
+	number_rows = int(available_space_y / (2 * alien_height))
+	return number_rows
+
+def create_alien(ai_settings, screen, aliens, alien_number, row_number):
+	#create an alien and place it in the row
+	alien = Alien(ai_settings, screen)
+	alien_width = alien.rect.width
+	alien.x = alien_width + 2 * alien_width * alien_number
+	alien.rect.x = alien.x
+	alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+	aliens.add(alien)
+
+def create_fleet(ai_settings, screen, ship, aliens):
+	alien = Alien(ai_settings, screen)
+	number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
+	number_rows = get_number_rows(ai_settings, ship.rect.height,
+		alien.rect.height)
+
+	#create the fleet
+	for row_number in range(number_rows):
+		for alien_number in range(number_aliens_x):
+			create_alien(ai_settings, screen, aliens, 
+				alien_number, row_number)
+
+def check_fleet_edges(ai_settings, aliens):
+	for alien in aliens.sprites():
+		if alien.check_edges():
+			change_fleet_direction(ai_settings, aliens)
+			break
+
+def change_fleet_direction(ai_settings, aliens):
+	#drop entire fleet and change direction
+	for alien in aliens.sprites():
+		alien.rect.y += ai_settings.fleet_drop_speed
+	ai_settings.fleet_direction *= -1
+
+def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
+	#respond to ship being hit
+	if stats.ships_left > 0:
+		stats.ships_left -= 1
+
+		aliens.empty()
+		bullets.empty()
+		create_fleet(ai_settings, screen, ship, aliens)
+		ship.center_ship()
+
+		sleep(0.5)
+	else:
+		stats.game_active = False
+
+def check_aliens_bottom(ai_settings,stats, screen, ship, aliens, bullets):
+	#check if any aliens have reached bottom of screen
+	screen_rect = screen.get_rect()
+	for alien in aliens.sprites():
+		if alien.rect.bottom >= screen_rect.bottom:
+			ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+			break
+
+def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
+	#update position of all aliens
+	check_fleet_edges(ai_settings, aliens)
+	aliens.update()
+
+	#look for alien-ship collisions
+	if pygame.sprite.spritecollideany(ship, aliens):
+		ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+		print("Ship hit!!")
+	check_aliens_bottom(ai_settings,stats, screen, ship, aliens, bullets)
