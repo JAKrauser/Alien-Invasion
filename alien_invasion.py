3ea595cc2ca777f569c46d1e78bab745a0c43af1
Krauser <krauser.ja@gmail.com>
2018-01-15 10:07:23 -0500

    new modules, refactoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import pygame
 
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -12,14 +13,29 @@ def run_game():
 	screen = pygame.display.set_mode(
 		(ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
-
 	ship = Ship(ai_settings, screen)
+	bullets = Group()
+	aliens = Group()
 
+	gf.create_fleet(ai_settings, screen, aliens)
+	
 	#Start main loop for the game.
 	while True:
 
-		gf.check_events(ship)
+		gf.check_events(ai_settings, screen, ship, bullets)
 		ship.update()
-		gf.update_screen(ai_settings,screen, ship)
+		gf.update_bullets(bullets)
+		gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+
+def check_events(ship, screen, ai_settings, bullets):
+	#Watch for keyboard and mouse events
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit()
+		elif event.type == pygame.KEYDOWN:
+			check_keydown_events(event, ship, screen, ai_settings, bullets)
+		elif event.type == pygame.KEYUP:
+			check_keyup_events(event, ship, screen, ai_settings, bullets)
+	
 
 run_game()
